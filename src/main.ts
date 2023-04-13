440ff92afe623df8838bb8b3fc986c006c5b79b9
mrbrunelli <matheus.brunelli@gmail.com>
2023-04-13 09:08:53 -0300

    refactor main, add single connection instance for app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
+import "dotenv/config";
 import express from "express";
+import { receitaController } from "./controller/ReceitaController";
+import { openConnection } from "./database/db";
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Servidor escutando em http://localhost:${port}`)
+app.use(express.json());
+app.use(receitaController);
 
-})
+openConnection()
+  .then(() => {
+    console.log("Banco de dados conectado");
+    app.listen(port, () => {
+      console.log(`Servidor escutando em http://localhost:${port}`);
+    });
+  })
+  .catch((e) => {
+    console.log(`Erro ao conectar banco de dados ${e.message}`);
+  });
